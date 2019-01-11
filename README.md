fc89ad9de9ccfe336a36b544d314b6b0c86a3b60
Steven F. Deloach Jr <sfdeloach@gmail.com>
2019-01-11 11:04:14 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Excavator
+
+## project goals
+1. Goal goes here
+2. Goal goes here
+3. Goal goes here
---
+++ src/org/altamonte/silver/Application.java
@@ -0,0 +1,14 @@
+package org.altamonte.silver;
+
+public class Application {
+
+	public Application() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hi Ho Silver! Away!");
+
+	}
+
+}
